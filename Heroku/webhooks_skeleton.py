fe1acd01a0b6978715533af22244479e4f173b88
Rahul Sathanapalli <skvrahul@gmail.com>
2017-10-27 11:21:41 +0530

    Updated webhook_skeleton

--- Heroku/webhooks_skeleton.py
+++ Heroku/webhooks_skeleton.py
@@ -3,18 +3,17 @@ import flask
 import telepot
 from flask import Flask, request
 import sys
-KEY = sys.argv[1]
 app = Flask(__name__)
-bot = telepot.Bot(KEY)
+bot = telepot.Bot('470225186:AAHY5h4DmqfSl12o9rrFH2FOlGev4E_X5XU')
 URL = '/bot_webhook'
-FULL_URL  = 'https://safe-journey-86486.herokuapp.com/bot_webhook'
+FULL_URL  = 'https://rocky-spire-61081.herokuapp.com/bot_webhook'
 
 def handle_update(update):
 	if 'message' in update:
 		msg_text = update['message']['text']
 		sender_id = update['message']['from']['id']
 		print(msg_text)
-		bot.sendMessage(sender_id, msg_text)
+		bot.sendMessage(sender_id, msg_text[::-1])
 
 @app.route(URL, methods=['GET', 'POST'])
 def pass_update():
@@ -23,9 +22,9 @@ def pass_update():
 	handle_update(update)
 	return 'OK'
 
+try:
+	bot.setWebhook(FULL_URL)
+except telepot.exception.TooManyRequestsError:
+	pass
 if __name__ == '__main__':
-	try:
-		bot.setWebhook(FULL_URL)
-	except telepot.exception.TooManyRequestsError:
-		pass
-	app.run(debug=True, port=443)
+	app.run()
